8f1b398e6e47ae700322a5e87869dfc92bffe7b6
AlishanSiddqui <99635375+inialishan@users.noreply.github.com>
2024-11-04 11:33:17 +0530

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -73,6 +73,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                 ]      
-            )      
+            ) 
+        ]
     buttons.append([InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID)])
     return buttons
